a1f566e82b8d1b070eb4800ce5eac36aed5e5171
Besher Kitaz <kitazb@berea.edu>
2025-05-21 13:24:31 -0400

    changed configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Head from './components/Head.jsx'
 import './css/App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Index from './components/Index.jsx'
 import About from './components/About.jsx'
