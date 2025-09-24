d6472b3d76d043f7cd5cd7ab68da951da9a92c02
Besher Kitaz <kitazb@berea.edu>
2025-09-24 00:09:07 -0400

    Renamed /dist to /docs to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs', // <-- change "dist" to "build" or whatever you like
+  },
   plugins: [react()],
   base: '/',
 })
