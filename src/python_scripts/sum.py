2fdbbe03ee16df9745a0b9d40e80f0659eeab470
Besher Kitaz <kitazb@berea.edu>
2025-09-23 23:43:51 -0400

    Deleted unnecessary files

--- src/python_scripts/sum.py
+++
@@ -1,8 +0,0 @@
-names = ["James", "Ali", "Bob", "Jose", "Mickle", "James", "Ali", "Ali", "James", "Jose"]
-
-
-names_count = {}
-
-for name in names:
-    names_count[name] = names_count.get(name, 0) + 1
-print(names_count)
